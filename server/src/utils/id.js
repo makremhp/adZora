58fd9f272b7da5a10bfd226a862875e858321fff
makremhp <ahhmed0983@gmail.com>
2026-09-19 17:22:44 +0100

    Delete server directory

--- server/src/utils/id.js
+++
@@ -1,7 +0,0 @@
-import crypto from "node:crypto";
-
-// Produces short, URL-safe, collision-resistant ids like "web_9f2ac1b3c4d5e6f7"
-export function makeId(prefix) {
-  const random = crypto.randomBytes(9).toString("base64url");
-  return `${prefix}_${random}`;
-}
